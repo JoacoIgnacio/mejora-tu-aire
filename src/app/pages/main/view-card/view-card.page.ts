de517f8b76508ae50d3294bb724be86b713edb89
Joaquín Ignacio <joaquin.carvajal@alumnos.ucn.cl>
2024-07-02 01:16:50 -0400

    Arreglo de descarga

--- src/app/pages/main/view-card/view-card.page.ts
+++ src/app/pages/main/view-card/view-card.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import * as pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-view-card',
@@ -12,9 +11,7 @@ export class ViewCardPage implements OnInit {
   card: any;
   checkboxValues: any = {}; // Objeto para almacenar los valores de las checkboxes
 
-  constructor(private route: ActivatedRoute) {
-    (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -47,98 +44,63 @@ export class ViewCardPage implements OnInit {
     };
   }
 
+  // Función para descargar la ficha como PDF
   downloadPDF() {
-    const docDefinition = {
-      content: [
-        { text: 'Ficha del Auto', style: 'header' },
-        { text: new Date().toLocaleString(), alignment: 'right' },
+    const doc = new jsPDF();
 
-        { text: 'Información del Cliente', style: 'subheader' },
-        {
-          columns: [
-            [
-              { text: `Patente: ${this.card.patente}` },
-              { text: `Número de OT: ${this.card.N_Ot}` },
-              { text: `ID Cliente: ${this.card.id_Cliente}` },
-              { text: `Nombre: ${this.card.name}` },
-              { text: `Fono: ${this.card.fono}` },
-              { text: `Dirección: ${this.card.direccion}` }
-            ],
-            [
-              { text: `Año del auto: ${this.card.year_Car}` },
-              { text: `Kilometraje: ${this.card.km}` },
-              { text: `Auto: ${this.card.car}` },
-              { text: `Marca: ${this.card.marca}` },
-              { text: `Modelo: ${this.card.modelo}` }
-            ]
-          ]
-        },
+    // Cambiar la fuente a Times New Roman
+    doc.setFont("times", "normal");
 
-        { text: 'Información del Servicio', style: 'subheader' },
-        {
-          table: {
-            widths: ['*', '*', '*', '*'],
-            body: [
-              [
-                { text: 'Filtro de polen cabina', bold: true },
-                { text: this.checkboxValues.filtro ? 'Sí' : 'No' },
-                { text: 'Prueba de fugas con gas nitrógeno', bold: true },
-                { text: this.checkboxValues.fugas ? 'Sí' : 'No' }
-              ],
-              [
-                { text: 'Vacío de sistema con bomba', bold: true },
-                { text: this.checkboxValues.vacio_bomba ? 'Sí' : 'No' },
-                { text: 'Inyección de aceite PAG sintético', bold: true },
-                { text: this.checkboxValues.inyeccion_aceite ? 'Sí' : 'No' }
-              ],
-              [
-                { text: 'Inyección de tinta UV', bold: true },
-                { text: this.checkboxValues.inyeccion_tinta ? 'Sí' : 'No' },
-                { text: `Carga de gas R134a: ${this.card.carga_gas_val} gr`, bold: true },
-                { text: this.checkboxValues.carga_gas ? 'Sí' : 'No' }
-              ],
-              [
-                { text: `Prueba de funcionamiento a: ${this.card.prueba_funcionamiento_val} rpm`, bold: true },
-                { text: this.checkboxValues.prueba_funcionamiento ? 'Sí' : 'No' },
-                { text: `Lecturas de T* de difusores de aire: ${this.card.lectura_difusores_val}`, bold: true },
-                { text: this.checkboxValues.lectura_difusores ? 'Sí' : 'No' }
-              ],
-              [
-                { text: `Lectura de presiones de sistema - Presión Baja: ${this.card.lectura_presiones_baja_val} psi`, bold: true },
-                { text: this.checkboxValues.lectura_presiones ? 'Sí' : 'No' },
-                { text: `Presión Alta: ${this.card.lectura_presiones_alta_val} psi`, bold: true },
-                { text: this.checkboxValues.lectura_presiones ? 'Sí' : 'No' }
-              ],
-              [
-                { text: 'Garantía de 3 meses', bold: true },
-                { text: this.checkboxValues.garantia ? 'Sí' : 'No' }
-              ]
-            ]
-          }
-        }
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          bold: true,
-          margin: [0, 0, 0, 10]
-        },
-        subheader: {
-          fontSize: 14,
-          bold: true,
-          margin: [0, 10, 0, 5]
-        },
-        tableExample: {
-          margin: [0, 5, 0, 15]
-        },
-        tableHeader: {
-          bold: true,
-          fontSize: 13,
-          color: 'black'
-        }
-      }
-    };
+    // Agregar un título
+    doc.setFontSize(18);
+    doc.text('Ficha del Vehículo', 105, 20, { align: 'center' });
+    doc.setFontSize(12);
 
-    pdfMake.createPdf(docDefinition).download(`Ficha_${this.card.N_Ot}.pdf`);
+    // Dibujar una línea debajo del título
+    doc.line(10, 25, 200, 25);
+
+    // Información del vehículo
+    doc.text(`Numero de OT: ${this.card.N_Ot}`, 10, 45);
+    doc.text(`Patente: ${this.card.patente}`, 10, 35);
+    doc.text(`ID Cliente: ${this.card.id_Cliente}`, 10, 55);
+    doc.text(`Nombre: ${this.card.name}`, 10, 65);
+    doc.text(`Fono: ${this.card.fono}`, 10, 75);
+    doc.text(`Dirección: ${this.card.direccion}`, 10, 85);
+    doc.text(`Año del auto: ${this.card.year_Car}`, 10, 95);
+    doc.text(`Kilometraje: ${this.card.km}`, 10, 105);
+    doc.text(`Auto: ${this.card.car}`, 10, 115);
+    doc.text(`Marca: ${this.card.marca}`, 10, 125);
+    doc.text(`Modelo: ${this.card.modelo}`, 10, 135);
+    doc.text(`Costo: ${this.card.costo}`, 10, 145);
+    doc.text(`Fecha: ${this.card.fecha}`, 10, 155);
+    doc.text(`Hora: ${this.card.hora}`, 10, 165);
+    doc.text(`Acciones Realizadas: ${this.card.acciones}`, 10, 175);
+
+    // Estado de los servicios
+   
+    doc.setFontSize(14);
+    doc.line(10, 25, 200, 25);
+    doc.text('Estado de los servicios:', 10, 185);
+    doc.setFontSize(12);
+    doc.text(`Filtro de polen cabina: ${this.checkboxValues.filtro ? 'Sí' : 'No'}`, 10, 195);
+    doc.text(`Prueba de fugas con gas nitrógeno: ${this.checkboxValues.fugas ? 'Sí' : 'No'}`, 10, 205);
+    doc.text(`Vacío de sistema con bomba: ${this.checkboxValues.vacio_bomba ? 'Sí' : 'No'}`, 10, 215);
+    doc.text(`Inyección de aceite PAG sintético: ${this.checkboxValues.inyeccion_aceite ? 'Sí' : 'No'}`, 10, 225);
+    doc.text(`Inyección de tinta UV: ${this.checkboxValues.inyeccion_tinta ? 'Sí' : 'No'}`, 10, 235);
+    doc.text(`Carga de gas R134a: ${this.checkboxValues.carga_gas ? this.checkboxValues.carga_gas_val + ' gr' : 'No'}`, 10, 245);
+    doc.text(`Prueba de funcionamiento: ${this.checkboxValues.prueba_funcionamiento ? this.checkboxValues.prueba_funcionamiento_val + ' rpm' : 'No'}`, 10, 255);
+    doc.text(`Lecturas de T* de difusores de aire: ${this.checkboxValues.lectura_difusores ? this.checkboxValues.lectura_difusores_val + ' lectura' : 'No'}`, 10, 265);
+    doc.text(`Lectura de presiones de sistema - Presión Baja: ${this.checkboxValues.lectura_presiones ? this.checkboxValues.lectura_presiones_baja_val + ' psi' : 'No'}`, 10, 275);
+    doc.text(`Lectura de presiones de sistema - Presión Alta: ${this.checkboxValues.lectura_presiones ? this.checkboxValues.lectura_presiones_alta_val + ' psi' : 'No'}`, 10, 285);
+    doc.text(`Garantía de 3 meses: ${this.checkboxValues.garantia ? 'Sí' : 'No'}`, 10, 295);
+
+    // Dibujar una línea al final del documento
+    doc.line(10, 300, 200, 300);
+
+    // Añadir la nota de creación del informe
+    doc.text('Informe creado por Mejora tu Aire', 105, 310, { align: 'center' });
+
+    // Guardar el PDF con un nombre específico
+    doc.save(`${this.card.N_Ot}.pdf`);
   }
 }
